d0544eebbdca96553a7056bcb6b015497b958226
OPHELIA1012 <150638639+OPHELIA1012@users.noreply.github.com>
2024-04-29 11:53:35 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var captureGraphics
+var radioElement
 var capture_width = 640
 var capture_height = 480
 var span = 5
@@ -15,8 +16,11 @@ var span = 5
   radioElement.option(width/2-300,20)
   radioElement.option("方塊")
   radioElement.option("圓圈")
+  radioElement.option("亮度")
+  radioElement.option("紅底")
+  radioElement.option("文字")
   radioElement.style("color","#fff")
-  //radioElement.style("fone-size","30")
+  radioElement.style("fone-size","30px")
 
 }
 
@@ -31,13 +35,40 @@ function draw() {
       for(var y=0; y<captureGraphics.height;y=y+span){
         var pixel = captureGraphics.get(x,y)
         fill(pixel)
-        if(radioElement.value()=="方塊"){
+        if(radioElement.value()=="方塊" || radioElement.value()==""){
         rect(x,y,span) 
       }
       if(radioElement.value()=="圓圈"){
         ellipse(x,y,span) 
       }
-       
+      if(radioElement.value()=="亮度"){
+        bk = (pixel[0]+pixel[1]+pixel[2])/3
+        fill(bk)
+        //ellipse(x,y,span)  //span代表圓圈的直徑
+        ellipse(x,y,span*map(bk,0,255,0,1))  //透過map指令，bk在0~255間，依照比率設定0~1r間的數字
+      }
+      if(radioElement.value()=="紅底"){
+        colorMode(HSB)
+        fill(pixel[0],80,80)
+      push()
+        translate(x,y)
+        rotate(pixel[0]/100)
+        rectMode(CENTER)
+        rect(0,0,span*0.6)
+        fill(0)
+        ellipse(0,0,10) 
+      pop()
+      }
+      if(radioElement.value()=="文字"){
+        const density = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
+        let txt = "一二三四五田雷電龕龘"
+        bk = (pixel[0]+pixel[1]+pixel[2])/3
+        let bkId = int(map(bk, 0, 255, 9, 0))
+        textSize(span)
+        textStyle(BOLD)
+        text(txt[bkId],x,y)
+      }
+
       }
     }
     pop()
