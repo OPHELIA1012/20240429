739e4643f089c42e0c33127dea17fad9bb81f89b
OPHELIA1012 <150638639+OPHELIA1012@users.noreply.github.com>
2024-04-29 10:38:16 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,31 @@
 var captureGraphics
+var capture_width = 640
+var capture_height = 480
+var span = 5
  function setup() {
   createCanvas(windowWidth, windowHeight);
   capture = createCapture(VIDEO) //啟動攝影機
-  capture.size(640,480); //設定顯示畫面大小
-  captureGraphics = captureGraphics(640,480)
-  captureGraphics.translate(640,0)
+  capture.size(capture_width,capture_height); //設定顯示畫面大小
+  captureGraphics = captureGraphics(capture_width,capture_height)
+  captureGraphics.translate(capture_width,0)
   captureGraphics.scale(-1,1)
-  captureGraphics.hide()
+  capture.hide()
 }
 
 function draw() {
   background(220);
+  noStroke
+  span = 5+map(mouseX,0,width,0,20)
   push()
-    translate(width/2-320,height/2-240) //把原點移到(width/2-320,height/2-240)
-    image(capture,0,0) //在(0,0)
+    translate(width/2-capture_width/2-capture_height/2) //把原點移到(width/2-320,height/2-240)
+    captureGraphics.image(capture,0,0) //在(0,0)
+    for (var x = 0; x<captureGraphics.width;x=x+span){
+      for(var y=0; y<captureGraphics.height;y=y+span){
+        var pixel = captureGraphics.get(x,y)
+        fill(pixel)
+        rect(x,y,span) 
+        //ellipse(x,y,span)
+      }
+    }
     pop()
 }
